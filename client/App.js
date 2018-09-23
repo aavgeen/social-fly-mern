4e3d037b5e23e0b31bb30c48631ef85c9b48975c
aavgeen1 <aavgeensingh1@gmail.com>
2018-09-24 00:24:06 +0530

    Initial  basic setup & configuration

---
+++ client/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const App = () => (
+  <div>Hello World. This is me, the Material Design Social Media.</div>
+)
+
+export default hot(module)(App)
